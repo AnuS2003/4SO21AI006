dc89265244906cf10f6a669294ddf8d7e5037633
AnuS2003 <21a04.anupama@sjec.ac.in>
2024-07-18 14:33:39 +0530

    Expected frontend code uploaded

--- problem1/src/App.js
+++ problem1/src/App.js
@@ -1,9 +1,29 @@
+import {useState} from 'react';
+import axios from 'axios';
+
+
 function App() {
   return (
+    const [ids, setIds] = useState('');
+    const [average, setAverage] = useState(null);
+    const [error, setError] = useState(null); 
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('http://localhost:5000/numbers', {ids: ids});
+        setAverage(response.data.average);
+      } catch (error) {
+        setError(error.response.data.error);
+      }
+    }
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+      <h1>Problem 1</h1>
+      <input type="text" value={ids}
+      onChange={(e) => setIds(e.target.value)} 
+      placeholder='Enter numbers'/>
+      <button onClick={fetchData}>Calculate</button>
+      {average !== null && <h2>Average: {average}</h2>}
+      {error && <h2>{error}</h2>}
     </div>
   );
 }
